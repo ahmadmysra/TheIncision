f6c9a2ac0078d90b6671feb2ed682575177dfc8f
Ahmad Mysra <ahmadmysra@users.noreply.github.com>
2021-02-06 23:25:25 +0200

    Update zerohour.bat
    make path a variable

--- zerohour.bat
+++ zerohour.bat
@@ -1,4 +1,5 @@
 @echo off
 set location=%1
+set path=%2
 
-ffmpeg -report -user_agent "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36" -protocol_whitelist "file,http,https,tcp,tls,crypto,data" -allowed_extensions ALL -i "http://link/to/m3u8/%location%.m3u8" -c copy "D:\the\full\path\%location%.ts"
+ffmpeg -report -user_agent "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36" -protocol_whitelist "file,http,https,tcp,tls,crypto,data" -allowed_extensions ALL -i "http://link/to/m3u8/%location%.m3u8" -c copy "%path%%location%.ts"
